4c94f34e81bf8f4358e246abd83c1b6bee90e21f
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-03-25 14:41:00 +0000

    Added handler function to get server running.
    Relates #12

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer(handler);
+
+const message = "hello!";
+
+function handler(request, response) {
+    
+    response.writeHead(200, { 'content-type': 'text/html'});
+    response.write(message);
+    response.end();
+}
 
 server.listen(3000, function () {
     console.log("Server is listening on port 3000!");
 });
-
