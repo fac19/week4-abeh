8d09a822dbeeebe0f730a7e4d01d96b154e95f82
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2020-03-26 15:24:01 +0000

    add tests for most handlers. Relates #8 Co-authored-by: Hettie <hettie.mcconnell@outlook.com>

--- test.js
+++ test.js
@@ -12,33 +12,59 @@ test("Check status code is 200 on homepage", t => {
   supertest(router)
     .get("/")
     .expect(200)
-    .expect(("content-type", /html/))
+    .expect("content-type", /html/)
     .end((error, response) => {
         t.error(error);
-        console.log('ATTENTION', response.body);
-    //   console.log("response.url", response.url)
-        t.equal(response.body.includes('<html lang'), '<html lang');      
+        t.equal(response.text.includes('<title>Blog Home</title>'), true);      
         t.end();
     });
-
-    // .end((error, response) => {
-    // //   console.log("response", response)
-    //   t.error(error);
-    //   console.log('ATTENTION', response.body);
-    // //   console.log("response.url", response.url)
-    //   t.equal(response.text, "index.html");      
-    //   t.end();
-    // });
 });
 
-// test("check status code is 200", t => {
+test("Check status code is 200 on form page", t => {
+  supertest(router)
+    .get("/displayForm")
+    .expect(200)
+    .expect("content-type", /html/)
+    .end((error, response) => {
+        t.error(error);
+        t.equal(response.text.includes('<title>Blog Post</title>'), true);      
+        t.end();
+    });
+});
+
+test("Check status code is 404 on incorrect url", t => {
+  supertest(router)
+    .get("/random")
+    .expect(404)
+    .expect("Content-type", /html/)
+    .end((error, response) => {
+      t.error(error);
+      t.equal(response.text, "<h1>Not found</h1>")
+      t.end()
+    });
+});
+
+test("Check status code is 302 on post creation", t => {
+  supertest(router)
+    .post("/create-post")
+    .expect(302)
+    .expect("location", "/")
+    .end((error, response) => {
+      t.error(error);
+      t.end();
+    })
+})
+
+// test("Blog route - post - password - payload", t => {
 //   supertest(router)
-//     .get("/")
-//     .expect(200)
-//     .expect("Content-Type", "text/plain")
+//     .post("/create-post")
+//     .send(["how are you"])
+//     .expect(302)
+//     .expect("Content-Type", "application/json")
 //     .end((err, res) => {
 //       t.error(err);
-//       t.equal(res.text, "Hello");
+//       t.deepEqual(res.body, ["how are you"]);
 //       t.end();
 //     });
 // });
+
