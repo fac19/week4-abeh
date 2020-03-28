1e60c3e895748343c585ca63f738b5def5e08615
Joe Jackson <joe.m.jackson@live.co.uk>
2020-03-28 12:29:10 +0000

    Delete files and restart project

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const hostname = process.env.HOSTNAME || "localhost";
-const port = process.env.PORT || 3001;
-
-const router = require("./router");
-
-http.createServer(router).listen(port, hostname, () => {
-  console.log(`Server running at port http://${hostname}:${port}`);
-});
